f14f124a0f11e1092ad5281d3bf5061bd99f5bd5
dibbo dey <dibbodey6000@gmail.com>
2025-05-22 13:37:14 +0600

    Add google cloud info there url

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -4,8 +4,9 @@ import os
 
 # You can use environment variables or hardcode this (not recommended)
 #DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:iamgreat@localhost:5432/AITutor")
-
-DATABASE_URL = "postgresql://postgres:abcd1234@localhost/AITutor"
+# Update DATABASE_URL to point to Cloud SQL
+DATABASE_URL = "postgresql://postgres:DEz(*5uf5\PaGGjl@34.134.252.6/AITutor"
+#DATABASE_URL = "postgresql://postgres:abcd1234@localhost/AITutor"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
